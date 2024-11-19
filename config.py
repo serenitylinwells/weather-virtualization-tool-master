3f9e1fd37667b37ac2fc7cc9d89a1500e0d3f94a
罗霄羽 <13310831343@163.com>
2024-11-19 11:30:03 +0800

    weather api business logic and security update

---
+++ config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    应用程序配置类，通过读取环境变量文件 `.env` 来加载配置项。
+
+    Attributes:
+        app_key (str): API接口密钥
+
+    Config:
+        env_file (str): 环境变量文件路径，默认为 `.env`。
+    """
+    app_key: str  # API接口密钥配置项
+
+    class Config:
+        env_file = ".env"  # 指定环境变量文件
+
+
+settings = Settings()
