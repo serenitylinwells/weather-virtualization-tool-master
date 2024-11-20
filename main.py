dae776bb5af7af6bf17c02db2e6f3545438d1b2e
罗霄羽 <13310831343@163.com>
2024-11-20 08:50:04 +0800

    fix: <main, config, weather> import error, url error, api key error fixed

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from controller.api.register import register_api
 from controller.api.weather import weather_api
 
 app = FastAPI()
-app.include_router(register_api, prefix="/weatherTool/register-api/", tags=["注册接口"])
-app.include_router(weather_api, prefix="/weatherTool/weather-api/", tags=["天气数据接口"])
+app.include_router(register_api, prefix="/weatherTool/register-api", tags=["注册接口"])
+app.include_router(weather_api, prefix="/weatherTool/weather-api", tags=["天气数据接口"])
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8081, reload=True)
