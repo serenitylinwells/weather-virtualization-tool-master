9250944e24d43512afcf0db609a65023d7926571
罗霄羽 <13310831343@163.com>
2024-12-05 17:17:15 +0800

    ！！！预警url变更 预警路由优化

--- controller/api/alarm.py
+++ controller/api/alarm.py
@@ -12,7 +12,7 @@ alarm_api = APIRouter()
 
 
 # 请求用户预警制定记录
-@alarm_api.get('/check-alarm/{username}')
+@alarm_api.get('/checkAlarm/{username}')
 async def alarm(username: str):
     """
     请求用户预警制定记录
@@ -33,7 +33,7 @@ async def alarm(username: str):
 
 
 # 添加预警信息
-@alarm_api.post('/set-alarm')
+@alarm_api.post('/setAlarm')
 async def add_alarm(user: UserDTO):
     """
     请求添加用户制定的预警信息
@@ -48,17 +48,11 @@ async def add_alarm(user: UserDTO):
 
     with open(alarm_json_path, 'r') as f:
         alarms = json.load(f)
-
-        # 在预警信息中检查用户制定的预警是否存在
-        if user.username in alarms:
-            for user_alarm in alarms[user.username]:
-                if user_alarm == user.detail:
-                    return ResponseModel(code=1, msg="相关预警已有定义", data={})
-            # 如果预警不存在，则添加到预警信息中
-            alarms[user.username].append(user.detail)
-        else:
-            # 如果用户不存在，则添加用户和预警到预警信息中
-            alarms[user.username] = [[user.detail]]
+        if user.username not in alarms:
+            alarms[user.username] = []
+        alarms[user.username].append(user.detail)
+        # 删除重复的预警信息
+        alarms[user.username] = list(set(alarms[user.username]))
 
     with open(alarm_json_path, 'w') as f:
         json.dump(alarms, f)
